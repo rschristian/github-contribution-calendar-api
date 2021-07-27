3abdb07e9d7e1677869bae0856717a2758aa72be
Ryan Christian <ryanchristian4427@gmail.com>
2021-07-27 14:18:36 -0500

    feat: Adding log output

--- workers/api/index.ts
+++ workers/api/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'worktop';
 import { listen } from 'worktop/cache';
 import cheerio from 'cheerio';
+import type { ServerRequest } from 'worktop/request';
 
 const API = new Router();
 
 API.add('GET', '/user/:username', async (req, res) => {
     const requestedUser = String(req.params.username);
+
+    await logToRocket(req, requestedUser)
 
     res.setHeader('Content-Type', 'application/json');
     res.setHeader('Cache-Control', 'public, max-age=86400, immutable');
@@ -17,6 +20,30 @@ API.add('GET', '/user/:username', async (req, res) => {
 });
 
 listen(API.run);
+
+async function logToRocket(req: ServerRequest, requestedUser: string) {
+    const data = {
+        'timestamp': new Date().toLocaleString('en-US', { timeZone: 'America/Chicago' }),
+        'ip': req.headers.get('cf-connecting-ip') || '',
+        'origin': req.headers.get('origin') && req.headers.get('origin') !== 'null'
+            ? req.headers.get('origin')
+            : req.headers.get('referer') && req.headers.get('referer') !== 'null'
+                ? req.headers.get('referer')
+                : '',
+        'user-agent': req.headers.get('user-agent') || '',
+        'city': req.cf.city || '',
+        'country': req.cf.country || '',
+        'requested-user': requestedUser,
+    }
+
+    await fetch(`https://logger.ryanchristian.dev/`, {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+}
 
 async function getUserData(requestedUser: string) {
     const data = await fetch(`https://github.com/users/${requestedUser}/contributions`);
