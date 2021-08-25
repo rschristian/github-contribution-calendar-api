66999d6c96b4179678ebfcdf6924bd152b50a857
Ryan Christian <ryanchristian4427@gmail.com>
2021-08-24 21:46:28 -0500

    refactor: Adding in @rschristian/twind-cli

---
+++ pages/dev/twind.config.js
@@ -0,0 +1,28 @@
+export default {
+    theme: {
+        extend: {
+            fontSize: {
+                '4xl': '2rem',
+                '5xl': '2.5rem',
+            },
+            height: {
+                '1/2': '50vh',
+                '3/4': '75vh',
+            },
+        },
+        colors: {
+            navy: '#253746',
+            offWhite: '#ddd',
+            red: {
+                DEFAULT: '#d14124',
+                hover: '#f17154',
+            },
+            steel: {
+                dimmer: '#b5b5b5',
+                dim: '#ddd',
+                DEFAULT: '#ededed',
+            },
+            white: '#fff',
+        },
+    },
+};
